51296529161bb15acf1283c1c0c6646629cda98c
Artiom Stadnijchuk <artiomstadnijchuk@gmail.com>
2025-04-30 14:01:41 +0500

    правки чекстайла (3)

--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -18,9 +17,8 @@ public class UserDto {
     @Email(groups = {OnCreate.class, OnUpdate.class}, message = "Некорректный email")
     private String email;
 
-    // Группы валидации
     public interface OnCreate {}
-    // Для POST
+
     public interface OnUpdate {}
-    // Для PATCH
+    
 }
